14e008082af62d80b308dc9563ba164e560675d5
Nikhilka4 <106381060+Nikhilka4@users.noreply.github.com>
2024-04-22 16:21:37 +0530

    main

--- build/app.js
+++ build/app.js
@@ -23,7 +23,7 @@ exports.app.use(express_1.default.json({ limit: "50mb" }));
 exports.app.use((0, cookie_parser_1.default)());
 // //cors
 exports.app.use((0, cors_1.default)({
-    origin: ["https://hitech-arge.netlify.app"],
+    origin: ["hitech-arge.netlify.app"],
     credentials: true,
 }));
 // api requests limit
